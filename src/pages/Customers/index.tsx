766578ef1ad15914f41f6520b3451892af865380
Uemerson Pinheiro Junior <uemersonpinheirojunior@gmail.com>
2021-03-06 16:05:07 -0300

    refactor: remove parseISO

--- src/pages/Customers/index.tsx
+++ src/pages/Customers/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 
-import { format, parseISO } from 'date-fns';
+import { format } from 'date-fns';
 import { Button, Grid, Breadcrumbs, Link, Typography } from '@material-ui/core';
 import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 import { useHistory } from 'react-router-dom';
