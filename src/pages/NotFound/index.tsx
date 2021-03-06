8f68ba428c5f885617fe4bd1b4d3f2250b2fd020
Uemerson Pinheiro Junior <uemersonpinheirojunior@gmail.com>
2021-03-06 17:00:02 -0300

    feat: add styles to page not found

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,5 +1,27 @@
 import React from 'react';
 
-const NotFound: React.FC = () => <h1>Not found</h1>;
+import { Grid, Typography, Link } from '@material-ui/core';
+
+import useStyles from './styles';
+
+const NotFound: React.FC = () => {
+  const classes = useStyles();
+
+  return (
+    <Grid
+      container
+      direction="column"
+      justify="center"
+      alignItems="center"
+      className={classes.body}
+    >
+      <Typography variant="h1">404</Typography>
+      <Typography variant="h2">Página não encontrada</Typography>
+      <Link href="/dashboard" color="inherit" variant="h6">
+        Voltar para o dashboard
+      </Link>
+    </Grid>
+  );
+};
 
 export default NotFound;
---
+++ src/pages/NotFound/styles.ts
@@ -0,0 +1,10 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(_ => ({
+  body: {
+    backgroundColor: '#FFF',
+    minHeight: '100vh',
+  },
+}));
+
+export default useStyles;
